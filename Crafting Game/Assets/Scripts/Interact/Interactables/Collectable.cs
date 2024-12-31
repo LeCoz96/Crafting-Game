99875502803f7eca084c42c937e432a98c16a232
LukyLewis2 <lewis.cozens@hotmail.co.uk>
2024-12-31 15:24:12 +0000

    + Icons and allocation to bar started

--- Crafting Game/Assets/Scripts/Interact/Interactables/Collectable.cs
+++ Crafting Game/Assets/Scripts/Interact/Interactables/Collectable.cs
@@ -1,5 +1,15 @@
+using UnityEngine;
+
+enum ItemType
+{
+    WOOD,
+    CLOTH
+}
+
 public class Collectable : InteractionManager
 {
+    [SerializeField] private ItemType _type;
+
     protected override void Interact()
     {
         gameObject.SetActive(false);
--- Crafting Game/Assets/Scripts/Player/SubSystems/InventoryBar/InventoryBarManager.cs
+++ Crafting Game/Assets/Scripts/Player/SubSystems/InventoryBar/InventoryBarManager.cs
@@ -1,16 +1,17 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class InventoryBarManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _item;
+    [SerializeField] private List<GameObject> _itemSlots;
+
+    private int value = 0;
 
     public void UpdateInventory(Sprite icon, int quantity)
     {
-        _item.GetComponent<InventoryBarItem>().UpdateIcon(icon);
-        _item.GetComponent<InventoryBarItem>().UpdateCurrentQuantity(quantity);
+        _itemSlots[value].GetComponent<InventoryBarItem>().UpdateIcon(icon);
+        _itemSlots[value].GetComponent<InventoryBarItem>().UpdateCurrentQuantity(quantity);
+
+        ++value;
     }
 }
